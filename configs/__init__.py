bca4913b19c2d3bded7f1ff9465082b33addf1a9
Flavio Arrigoni <f.arrigoni@student.tugraz.at>
2025-05-12 16:54:05 +0200

    datapath debug

--- configs/__init__.py
+++ configs/__init__.py
@@ -82,7 +82,7 @@ class DatasetParams(ParamGroup):
 
     def __init__(self, parser):
         self.dataset = "colmap"
-        self.data_path = "/mnt/hdd/team4/data"
+        self.data_path = "data/mipnerf360"
         self.scene = "bonsai"
         self.patch_based = False
         self.downsample = [4, 2, 1]
