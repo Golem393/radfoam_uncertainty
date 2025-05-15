ba5d250415d87b0e803204102f730103d809ad4a
Flavio Arrigoni <f.arrigoni@student.tugraz.at>
2025-05-15 17:48:50 +0200

    uncertainty_test debug

--- uncertainty.py
+++ uncertainty.py
@@ -5,8 +5,7 @@ from configs import *
 from radfoam_model.scene import RadFoamScene
 from nerfstudio.field_components.encodings import HashEncoding
 from data_loader import DataHandler
-from radfoam_model.utils import (find_grid_indices,
-                                        normalize_point_coords)
+from utils import (find_grid_indices, normalize_point_coords)
 import tqdm
 
 class ComputeUncertainty:
