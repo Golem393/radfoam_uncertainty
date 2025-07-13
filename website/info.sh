5fff5ed5f2c4e3a555ac87f2d3d9a0be17c4e41b
Flavio Arrigoni <f.arrigoni@student.tugraz.at>
2025-07-13 18:19:36 +0200

    renamed folder

--- website/info.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p videos_h264
-
-for f in videos/*.mp4; do
-    base=$(basename "$f")
-    ffmpeg -i "$f" -c:v libx264 -crf 23 -preset fast -an "videos_h264/$base"
-done
--- website/info2.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p videos_640p
-
-for f in videos/*.mp4; do
-    base=$(basename "$f")
-    ffmpeg -i "$f" -vf "scale=640:-2" -c:v libx264 -crf 23 -preset medium -profile:v high -level 4.0 -movflags +faststart -an "videos_640p/$base"
-done
